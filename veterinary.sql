7dc5ca42a225d42774e80f8459908eea30dba677
Jake Laylo <laylojake01@gmail.com>
2023-11-09 15:22:52 +0800

    feat create table animals

--- veterinary.sql
+++ veterinary.sql
@@ -8,3 +8,15 @@ CREATE TABLE owners(
     phone VARCHAR(15) NOT NULL,
     email VARCHAR(100) NOT NULL
 );
+
+CREATE TABLE animals(
+    animal_id INT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    species VARCHAR(50) NOT NULL,
+    breed VARCHAR(50) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    gender VARCHAR(10) NOT NULL,
+    color VARCHAR(50) NOT NULL,
+    owner_id INT NOT NULL,
+    FOREIGN KEY (owner_id) REFERENCES owners(owner_id)
+);
